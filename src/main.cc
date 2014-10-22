bd1093883d36627e601a65bcf5fb3b1f0a1cb389
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2014-10-22 18:14:32 -0200

    Using OgreScene

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,6 @@
 
 #include <ugdk/system/engine.h>
-#include <ugdk/action/scene.h>
+#include <ugdk/action/3D/ogrescene.h>
 #include <ugdk/input/events.h>
 #include <memory>
 
@@ -10,12 +10,13 @@ int main(int argc, char* argv[]) {
     ugdk::system::Configuration config;
     config.base_path = "assets/";
     ugdk::system::Initialize(config);
-    ugdk::action::Scene* ourscene = new ugdk::action::Scene;
-    ourscene->event_handler().AddListener<ugdk::input::KeyPressedEvent>(
-        [ourscene](const ugdk::input::KeyPressedEvent& ev) {
-            if(ev.scancode == ugdk::input::Scancode::ESCAPE)
-                ourscene->Finish();
-        });
+    ugdk::action::Scene* ourscene = new ugdk::action::mode3d::OgreScene;
+    double delay = 2.0;
+    ourscene->AddTask(ugdk::system::Task(
+        [ourscene, &delay](double dt) {
+          if ((delay -= dt) < 0)
+              ourscene->Finish();
+        }));
     ugdk::system::PushScene(unique_ptr<ugdk::action::Scene>(ourscene));
     ugdk::system::Run();
     ugdk::system::Release();
