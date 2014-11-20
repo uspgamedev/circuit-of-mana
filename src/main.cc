5ee28737524ae397531f6856ee7723c07a1fc3cb
Fernando Omar Aluani <rewasvat@uspgamedev.org>
2014-11-20 14:47:09 -0200

    Refactored object creation to functions and added a ground plane and gravity to the scene.

--- src/main.cc
+++ src/main.cc
@@ -18,7 +18,7 @@
 #include <OgreConfigFile.h>
 #include <OgreMeshManager.h>
 
-#define AREA_RANGE 20.0
+#define AREA_RANGE 200.0
 
 using std::unique_ptr;
 
@@ -27,45 +27,70 @@ bulletworks::PhysicScene *ourscene;
 #define BIT(x) (1<<(x))
 enum CollisionGroup {
     HEADS = BIT(6),
-    WAT1 = BIT(7),
+    WALLS = BIT(7),
     WAT2 = BIT(8),
     WAT3 = BIT(9),
     WAT4 = BIT(10)
 };
 
+bulletworks::Object* createOgreHead(const std::string& name, bool useBox=false) {
+    Ogre::SceneManager *mSceneMgr = ourscene->manager();
+    bulletworks::Object::PhysicsData headData;
+    auto headEnt = mSceneMgr->createEntity(name, "ogrehead.mesh");
+    auto meshShapeConv = BtOgre::StaticMeshToShapeConverter(headEnt);
+    if (useBox)
+        headData.shape = meshShapeConv.createBox();
+    else
+        headData.shape = meshShapeConv.createSphere();
+    headData.mass = 10;
+    headData.collision_group = CollisionGroup::HEADS;
+    headData.collides_with = CollisionGroup::WALLS | CollisionGroup::HEADS;
+    auto head = new bulletworks::Object(headEnt, headData);
+    head->AddToScene(ourscene);
+    head->Scale(0.4, 0.4, 0.4);
+    return head;
+}
+
+bulletworks::Object* createWall(const std::string& name, const Ogre::Vector3& dir, double dist, double width = AREA_RANGE, double height = AREA_RANGE) {
+    Ogre::SceneManager *mSceneMgr = ourscene->manager();
+    Ogre::Plane plane(dir, dist);
+
+    Ogre::MeshManager::getSingleton().createPlane(name, Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
+        plane, width, height, 5, 5, true, 1, 5, 5, dir.perpendicular());
+
+    const std::string wat = name + "Entity";
+    Ogre::Entity* wallEnt = mSceneMgr->createEntity(wat, name);
+    wallEnt->setMaterialName("Ogre/Tusks");
+    
+    bulletworks::Object::PhysicsData wallData;
+    wallData.shape = new btStaticPlaneShape(btVector3(dir.x, dir.y, dir.z), dist);
+    wallData.mass = 0;
+    wallData.collision_group = CollisionGroup::WALLS;
+    wallData.collides_with = CollisionGroup::HEADS;
+
+    auto wall = new bulletworks::Object(wallEnt, wallData);
+    wall->AddToScene(ourscene);
+    wall->body()->setRestitution(0.6);
+    return wall;
+}
+
 int main(int argc, char* argv[]) {
     ugdk::system::Configuration config;
     config.base_path = "assets/";
     ugdk::system::Initialize(config);
-    ourscene = new bulletworks::PhysicScene(btVector3(0, 0, 0));
+    ourscene = new bulletworks::PhysicScene(btVector3(0, -10, 0));
     ourscene->physics_manager()->set_debug_draw_enabled(true);
 
-    Ogre::SceneManager *mSceneMgr = ourscene->manager();
-
-    bulletworks::Object::PhysicsData head1data;
-    auto head1ent = mSceneMgr->createEntity("Head", "ogrehead.mesh");
-    auto meshShapeConv = BtOgre::StaticMeshToShapeConverter(head1ent);
-    head1data.shape = meshShapeConv.createSphere();
-    head1data.mass = 10;
-    head1data.collision_group = CollisionGroup::HEADS;
-    head1data.collides_with = CollisionGroup::HEADS;
-    auto head1 = new bulletworks::Object(head1ent, head1data);
-    head1->AddToScene(ourscene);
-    head1->Scale(0.5, 0.5, 0.5);
-
-    bulletworks::Object::PhysicsData head2data;
-    head2data.shape = meshShapeConv.createBox();
-    head2data.mass = 10;
-    head2data.collision_group = CollisionGroup::HEADS;
-    head2data.collides_with = CollisionGroup::HEADS;
-    auto head2 = new bulletworks::Object(mSceneMgr->createEntity("Head2", "ogrehead.mesh"), head2data);
-    head2->AddToScene(ourscene);
+    auto head1 = createOgreHead("Head");
+    auto head2 = createOgreHead("Head2", true);
     head2->Translate(0, 0, 80);
-    head2->Scale(0.5, 0.5, 0.5);
+    head2->body()->setAngularFactor(btVector3(0.0, 0.0, 0.0));
 
     ourscene->camera()->AttachTo(head2);
     ourscene->camera()->SetParameters(Ogre::Vector3::ZERO, 5000);
-    //ourscene->camera()->SetDistance(50);
+    ourscene->camera()->SetDistance(100);
+
+    createWall("ground", Ogre::Vector3::UNIT_Y, -(AREA_RANGE / 2));
 
     ourscene->event_handler().AddListener<ugdk::input::KeyPressedEvent>(
         [head2] (const ugdk::input::KeyPressedEvent& ev) -> void {
@@ -81,7 +106,7 @@ int main(int argc, char* argv[]) {
                 ourscene->Finish();
         });
 
-    mSceneMgr->setAmbientLight(Ogre::ColourValue(0.6, .6, .6));
+    ourscene->manager()->setAmbientLight(Ogre::ColourValue(0.7, .7, .7));
 
     ugdk::system::PushScene(unique_ptr<ugdk::action::Scene>(ourscene));
     ugdk::system::Run();
